31bb344bc7c5cc4d85c99087aa6f3944a170b830
lTMeol <thinh123haiba@gmail.com>
2025-07-17 09:39:27 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Detail from './pages/Detail';
+// import History from './pages/History';
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/detail" element={<Detail />} />
+//         <Route path="/history" element={<History />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+// App.jsx
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import History from './pages/History';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
   );
 }
+
+
--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,13 @@
 //   plugins: [react()],
 // })
 // vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/weather-app/', // <- CHỈNH base theo tên repo GitHub
+  base: '/weather-app/', 
   plugins: [react()],
 })
+
 
